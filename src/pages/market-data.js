7818239093d783eb317f8b7d40e572021515ead2
Arunan <arunan_cs@yahoo.com>
2022-05-05 06:45:05 +0000

    Adding netlify function

--- src/pages/market-data.js
+++ src/pages/market-data.js
@@ -1,15 +1,25 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import Button from "@mui/material/Button";
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { invokeHelloFunction } from '../services/invokeFunctionService'
+function invokefunction() {
 
+  invokeHelloFunction();
+
+}
 const MarketData = () => (
   <Layout>
     <Seo title="Market Data" />
     <h1>Market Data</h1>
     <p>Market Data Table comes here.</p>
     <Link to="/">Go back to the homepage</Link>
+    <Button
+      onClick={invokefunction}
+      color="inherit"
+    > Get Data </Button>
   </Layout>
 )
 
