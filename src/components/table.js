8de6d60bf6d4df15c2b3b4067cf5668b95b99089
Arunan <arunan_cs@yahoo.com>
2022-05-08 22:32:01 +0000

    Historical market data table with mock

---
+++ src/components/table.js
@@ -0,0 +1,197 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import { visuallyHidden } from '@mui/utils';
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
+
+function getComparator(order, orderBy) {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+// This method is created for cross-browser compatibility, if you don't
+// need to support IE11, you can use Array.prototype.sort() directly
+function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) {
+            return order;
+        }
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+}
+
+const EnhancedTableToolbar = (props) => {
+    const { tableToolbarHeader } = props;
+
+    return (
+        <Toolbar
+            sx={{
+                pl: { sm: 2 },
+                pr: { xs: 1, sm: 1 }
+            }}
+        >
+            <Typography
+                sx={{ flex: '1 1 100%' }}
+                variant="h6"
+                id="tableTitle"
+                component="div"
+            >
+                {tableToolbarHeader}
+          </Typography>
+        </Toolbar>
+    );
+};
+
+function EnhancedTableHead(props) {
+    const { order, orderBy, onRequestSort, headCells } =
+        props;
+    const createSortHandler = (property) => (event) => {
+        onRequestSort(event, property);
+    };
+
+    return (
+        <TableHead>
+            <TableRow>
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <Box component="span" sx={visuallyHidden}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                </Box>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+EnhancedTableHead.propTypes = {
+    onRequestSort: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired
+};
+
+const EnhancedTable = ({ enableSelection, tableData, headCells, tableToolbarHeader }) => {
+    console.log(enableSelection);
+    console.log(tableData);
+    const rows = tableData.results;
+    const [order, setOrder] = React.useState('asc');
+    const [orderBy, setOrderBy] = React.useState('calories');
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    // Avoid a layout jump when reaching the last page with empty rows.
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Paper sx={{ width: '100%', mb: 2 }}>
+                <EnhancedTableToolbar tableToolbarHeader={tableToolbarHeader} />
+                <TableContainer>
+                    <Table
+                        sx={{ minWidth: 750 }}
+                        aria-labelledby="tableTitle"
+                    >
+                        <EnhancedTableHead
+                            order={order}
+                            orderBy={orderBy}
+                            onRequestSort={handleRequestSort}
+                            headCells={headCells}
+                        />
+                        <TableBody>
+                            {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+                 rows.slice().sort(getComparator(order, orderBy)) */}
+                            {stableSort(rows, getComparator(order, orderBy))
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row, index) => {
+
+                                    return (
+                                        <TableRow hover>
+                                            <TableCell align="left">{row.date}</TableCell>
+                                            <TableCell align="right">{row.open}</TableCell>
+                                            <TableCell align="right">{row.high}</TableCell>
+                                            <TableCell align="right">{row.low}</TableCell>
+                                            <TableCell align="right">{row.close}</TableCell>
+                                            <TableCell align="right">{row.volume}</TableCell>
+                                            <TableCell align="right">{row.adjClose}</TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                            {emptyRows > 0 && (
+                                <TableRow
+                                    style={{
+                                        height: (53) * emptyRows,
+                                    }}
+                                >
+                                    <TableCell colSpan={6} />
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Paper>
+        </Box>
+    );
+}
+
+export default EnhancedTable;
--- src/pages/historical-data.js
+++ src/pages/historical-data.js
@@ -1,16 +1,211 @@
 import * as React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import EnhancedTable from "../components/table"
+import marketData from "../marketData"
 
-const HistoricalData = () => (
-  <Layout>
-    <Seo title="Historical Data" />
-    <h1>Historical Market Data</h1>
-    <p>Historical Market Data Table comes here</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const headCells = [
+  {
+    id: 'date',
+    numeric: false,
+    label: 'Date',
+  },
+  {
+    id: 'open',
+    numeric: true,
+    label: 'Open',
+  },
+  {
+    id: 'high',
+    numeric: true,
+    label: 'High',
+  },
+  {
+    id: 'low',
+    numeric: true,
+    label: 'Low',
+  },
+  {
+    id: 'close',
+    numeric: true,
+    label: 'Close Price',
+  },
+  {
+    id: 'volume',
+    numeric: true,
+    label: 'Volume',
+  },
+  {
+    id: 'adjClose',
+    numeric: true,
+    label: 'Adjusted Closing Price',
+  },
+];
+
+
+const symbols = [
+  { label: 'TECHM.NS', symbol: 'TECHM.NS' },
+  { label: 'MARUTI.NS', symbol: 'MARUTI.NS' },
+  { label: 'ONGC.NS', symbol: 'ONGC.NS' },
+  { label: 'ICICIBANK.NS', symbol: 'ICICIBANK.NS' },
+  { label: 'WIPRO.NS', symbol: 'WIPRO.NS' },
+  { label: 'ULTRACEMCO.NS', symbol: 'ULTRACEMCO.NS' },
+  { label: 'TITAN.NS', symbol: 'TITAN.NS' },
+  { label: 'SHREECEM.NS', symbol: 'SHREECEM.NS' }
+]
+
+const formatData = date => date.toISOString().slice(0, 10);
+
+const HistoricalData = () => {
+
+  const [symbolList, setSymbolList] = React.useState('');
+  const [startDate, setStartDate] = React.useState(null);
+  const [endDate, setEndDate] = React.useState(null);
+  const [openSnack, setOpenSnack] = React.useState(false);
+  const [alertSeverity, setAlertSeverity] = React.useState('');
+  const [historicalMarketData, setHistoricalMarketData] = React.useState(null);
+
+  const [tableToolbarHeader, setTableToolbarHeader] = React.useState('');
+
+  const vertical = 'bottom';
+  const horizontal = 'right';
+
+  const openSnackbar = (alertSeverity) => {
+    setOpenSnack(true);
+    setAlertSeverity(alertSeverity);
+  };
+
+  const closeSnackbar = () => {
+    setOpenSnack(false);
+  };
+
+  const searchData = () => {
+    if (symbolList && startDate & endDate) {
+      console.log(symbolList.symbol)
+      console.log(formatData(startDate))
+      console.log(formatData(endDate))
+      setTableToolbarHeader(`Symbol : ${symbolList.symbol} | Date: From ${formatData(startDate)} To ${formatData(endDate)}`)
+      setHistoricalMarketData(marketData);
+      console.log(historicalMarketData);
+
+    } else {
+      openSnackbar('warning');
+    }
+  }
+
+  return (
+    <Layout>
+      <Seo title="Historical Data" />
+      <Typography
+        component="h2"
+        variant="h4"
+        color="inherit"
+        noWrap
+        sx={{ flexGrow: 1, mb: 2 }}
+      >
+        {'Historical Market Data'}
+      </Typography>
+      <Grid container spacing={3} sx={{ mb: 4 }}>
+        <Grid item xs={12} md={12} lg={12}>
+          <Paper
+            sx={{
+              p: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              height: '100%'
+            }}
+          >
+            <Grid container spacing={3} item xs={12} md={12} lg={12}>
+              <Grid item xs={12} md={12} lg={12}>
+                <Typography
+                  component="h3"
+                  variant="h5"
+                  color="inherit"
+                  noWrap
+                  sx={{ flexGrow: 1, mb: 1 }}
+                >
+                  {'Search Historical Market Data'}
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={12} lg={3}>
+                <Autocomplete
+                  autoHighlight
+                  disablePortal
+                  id="symbol"
+                  options={symbols}
+                  onChange={(event, newValue) => {
+                    setSymbolList(newValue);
+                  }}
+                  renderInput={(params) => <TextField required {...params} label="Symbol" />}
+                />
+              </Grid>
+              <Grid item xs={12} md={12} lg={3}>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DatePicker
+                    id="startDate"
+                    label="Start Date"
+                    value={startDate}
+                    onChange={(newValue) => {
+                      setStartDate(newValue);
+                    }}
+                    renderInput={(params) => <TextField required sx={{ width: '100%' }} {...params} />}
+                  />
+                </LocalizationProvider>
+              </Grid>
+              <Grid item xs={12} md={12} lg={3}>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DatePicker
+                    id="endDate"
+                    label="End Date"
+                    value={endDate}
+                    onChange={(newValue) => {
+                      setEndDate(newValue);
+                    }}
+                    renderInput={(params) => <TextField required sx={{ width: '100%' }} {...params} />}
+                  />
+                </LocalizationProvider>
+              </Grid>
+              <Grid item xs={12} md={12} lg={3}>
+                <Button variant="contained" onClick={searchData}>Search</Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+      {historicalMarketData && (
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={12} lg={12}>
+            <EnhancedTable enableSelection={true} headCells={headCells} tableData={historicalMarketData} tableToolbarHeader={tableToolbarHeader} />
+          </Grid>
+        </Grid>
+      )}
+
+      <Snackbar open={openSnack} autoHideDuration={5000} onClose={closeSnackbar} anchorOrigin={{ vertical, horizontal }} key={vertical + horizontal}>
+        <Alert onClose={closeSnackbar} severity={alertSeverity} sx={{ width: '100%' }}>
+          {'Please enter required fields'}
+        </Alert>
+      </Snackbar>
+
+    </Layout>
+  )
+}
 
 export default HistoricalData
